c9a0089ce98822594411b2be9df307133b1b9c1f
dpoconnell <dpoconnell18@mail.strakejesuit.org>
2017-05-30 19:44:59 -0500

    Uploaded Real Files

---
+++ fractal_Tree.java
@@ -0,0 +1,29 @@
+package fractal_Tree;
+
+import java.awt.Graphics;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Canvas;
+
+public class fractal_Tree extends Canvas {
+	
+	public fractal_Tree() {
+		setBackground(Color.WHITE);
+	}
+
+	public void paint(Graphics window) {
+		window.setColor(Color.BLUE);
+		window.drawString("Serpinski's Gasket", 25, 50);
+
+		
+	}
+
+	public void triangle(Graphics window, int x1, int y1, int x2, int y2, int x3, int y3, int r) {
+
+	}
+
+	public void run() {
+		
+		}
+	
+}
---
+++ fractal_Tree_Runner.java
@@ -0,0 +1,14 @@
+package fractal_Tree;
+
+
+
+public class fractal_Tree_Runner {
+	
+	public fractal_Tree_Runner() {
+		
+	}
+
+	public static void main(String args[]) {
+		
+	}
+}
